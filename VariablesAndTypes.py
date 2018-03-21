d3eeb4f4e56b3f1a4fd1d2cc335c611c8c313e05
hello7s <strugglemonkey@gmail.com>
2018-03-21 11:42:57 -0400

    Variables and Types)

---
+++ VariablesAndTypes.py
@@ -0,0 +1,37 @@
+#Python supports two types of numbers: integers and floating point numbers.
+
+myint = 100
+print(myint)
+
+#Either of the following notations work for floating point numbers
+myfloat = 100.0
+print(myfloat)
+
+myfloat = float(100)
+print(myfloat)
+
+#Strings use either single or double quotes
+mystring = 'Hola'
+
+yourstring = "Adios"
+
+print(mystring,yourstring)
+
+one = 1
+two = 2
+three = one + two
+
+print(one, two, three)
+
+watch = "Watch the space"
+space = " "
+between = "between the words"
+print(watch,space,between)
+print(watch,between)
+print(watch + between)
+print (watch + " " + between)
+
+#Variable assignments can be done with multiple variables
+x, y = 100,200
+print(x,y)
+print (x+y)
