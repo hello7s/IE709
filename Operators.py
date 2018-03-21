2a35c3f3cb777bad3fd5cd5b63bacbf1accd446a
hello7s <strugglemonkey@gmail.com>
2018-03-21 12:22:36 -0400

    Operators and Lists

--- Operators.py
+++ Operators.py
@@ -36,4 +36,4 @@ print("big_list contains %d objects" % len(big_list))
 if x_list.count(x) == 10 and y_list.count(y) == 10:
     print("Almost there...")
 if big_list.count(x) == 10 and big_list.count(y) == 10:
-    print("Great!")
+    print("Great Job!")
